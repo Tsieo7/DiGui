76ac79af31d1b33ca06b63ac185d5671e986b6d3
Tsieo7 <245921781@qq.com>
2017-10-19 15:18:18 +0800

    递归和迭代
    1.斐波那契数列中任一项的平方数都等于跟它相邻的前后两项的乘积加1或减1;
    2.任取相邻的四个斐波那契数，中间两数之积（内积）与两边两数之积（外积）相差1.

---
+++ DiGui.java
@@ -0,0 +1,43 @@
+﻿//兔子问题 同斐波那契数列
+import java.util.Scanner;
+
+import org.junit.Test;
+
+public class DiGui {
+	//迭代算法
+//	@Test
+//	public void tuzi(){
+//
+//		int[] a;
+//		a=new int[40];
+//		a[0]=0;
+//		a[1]=1;
+//		for (int i = 2; i < 40; i++) {
+//			a[i]=a[i-1]+a[i-2];
+//			System.out.println("this is "+ a[i]);
+//		}
+//	}
+//		
+
+	
+//递归算法
+	int tuzi1(int i){
+
+		if(i<2)
+			{return i==0?0:1;}
+		return tuzi1(i-1)+tuzi1(i-2);
+
+	}
+	public static void main(String [] args){
+		Integer a;
+		DiGui d=new DiGui();
+		Scanner s=new Scanner(System.in);
+		while(s.hasNext()){
+			a=s.nextInt();
+			System.out.println(d.tuzi1(a));
+			}
+		
+		
+	}
+	}
+
